57998abcd66d7557350b0a893964fd4c8e86bbd7
Samantha Jordan <shinyguns@gmail.com>
2020-03-31 17:26:42 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 CREATE TABLE projects (id INTEGER PRIMARY KEY, 
                         title TEXT, 
                         category field TEXT, 
@@ -13,3 +14,8 @@ CREATE TABLE pledges (id INTEGER PRIMARY KEY,
                     amount INTEGER,
                     user_id TEXT,
                     project_id TEXT);
+=======
+ ```sql
+CREATE TABLE projects (id INTEGER PRIMARY KEY);
+```
+>>>>>>> 1c50fb0de962e57f2d3ecc4f79b17ab3ddb6ec3b
